af9bd05e1fbec396bca8562c2a63d15eb4755a96
HyunJoo Kim <hyunjoogo@gmail.com>
2021-10-31 03:21:36 +0900

    morgan 적용
    미들웨어 라이브러리 morgan 적용

--- src/server.js
+++ src/server.js
@@ -1,41 +1,22 @@
 import express from "express";
+import morgan from 'morgan';
 
 const PORT = 4000;
 
 const app = express();
+const loggerMiddleware = morgan("dev");
 
-
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to ${req.url}`);
-  next();
-}
-
-const handleHome = (req, res) => {
+const home = (req, res) => {
   return res.end();
 }
 
-const mustMiddleware = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const login = (req, res) => {
+  return res.end();
 }
 
-const handleProtected = (req, res) => {
-  return res.send('Hi! 프라이빗 라운지입니다.')
-}
-
-const protectedThis = (req, res, next) => {
-  const url = req.url;
-  if (url === '/protected') {
-    return res.send('Not Allowd');
-  }
-  next()
-}
-
-app.use(mustMiddleware);
-app.use(protectedThis);
-
-app.get('/', gossipMiddleware, handleHome);
-app.get('/protected', handleProtected);
+app.use(loggerMiddleware);
+app.get('/', home);
+app.get('/login', login);
 
 const handleListening = () => console.log('Server listening for 4000 : 🚀');
 
