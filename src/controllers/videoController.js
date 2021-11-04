00ec2e109f46bea9f6ca2a73088fa8729dca06be
hyunjoogo <hyunjoogo@gmail.com>
2021-11-05 02:42:46 +0900

    Templates
    PUG
    1. 확장과 상속개념
    2. 변수명으로 JS 사용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,11 @@
 export const trending = (req, res) => {
-  res.render('watch' ,{message : 'Watch! Video!'});
+  res.render("home", {pageTitle : "Home"});
 }
 export const see = (req, res) => {
-  return res.send(`Watch the Video #${req.params.id}`);
+  res.render("watch");
 }
 export const edit = (req, res) => {
-  console.log(req.params)
-  return res.send(`Edit the Video #${req.params.id}`);
+  res.render("edit");
 }
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
